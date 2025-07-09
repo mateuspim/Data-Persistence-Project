f3c7b95e37be94f0688de9c13c2099a8ee208736
mateuspim <mateuspimsantos@gmail.com>
2025-07-09 15:26:47 -0300

    Add a way to store the curr Player Name

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -13,10 +13,18 @@ public class MenuUIHandler : MonoBehaviour
 {
     [SerializeField]
     private Text MainMenuText;
+    [SerializeField]
+    private InputField playerNameInputField;
 
     public void Start()
     {
         UpdateText();
+        playerNameInputField.onValueChanged.AddListener(UpdateCurrPlayer);
+    }
+
+    private void UpdateCurrPlayer(string value)
+    {
+        GameManager.Instance.currPlayerName = value;
     }
     public void UpdateText()
     {
